197110ecab3cc0f2e96d9960b535bd9003fecc24
ankittttraj <115062391+ankittttraj@users.noreply.github.com>
2022-10-05 11:51:03 +0530

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2022 All rights reserved, Food House. Developed By - <a href="https://www.linkedin.com/in/surya-kant-singh-17a1421b1/"> Surya Kant Singh</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
